9ce421f812b19651c3edf38ca39e30070903e29d
unknown <omosiyobo@gmail.com>
2022-07-03 00:56:07 +0100

    8

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,11 @@ config()
 
 async function connect(uri) {
   try {
-    mongoose.connect(uri || process.env.MONGO_DB_LOCAL)
-    console.log('connected to database')
+    mongoose.connect(uri || process.env.MONGO_DB_LOCAL);
+    console.log('connected to database');
   } catch(err) {
-    console.log(err.message)
+    console.log(err.message);
+    
   }
 }
 
